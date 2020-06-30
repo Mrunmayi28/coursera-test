62ef4e2fb71cf28344983ee9c16d212ea09e07ea
Mrunmayi28 <mrpatankar28@gmail.com>
2020-06-30 18:28:46 +0530

    Fourth Assignment

---
+++ Assignment4/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+var speakWord = "Good Bye";
+
+
+
+(function(window) {
+	var byeSpeaker={}
+		byeSpeaker.speak= function(name){
+ 	 		console.log(speakWord + " " + name);
+		}
+window.byeSpeaker=byeSpeaker;
+})(window);
